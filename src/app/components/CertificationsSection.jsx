ed70709f69e23202f148a884d72676d11bfefb65
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-02-11 20:40:37 -0300

    fix: Fix image bug on deploy

--- src/app/components/CertificationsSection.jsx
+++ src/app/components/CertificationsSection.jsx
@@ -3,13 +3,55 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image'
 
-import { images } from '../../../public/certificationsImages/certificationsImages';
-
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { EffectCoverflow, Navigation, Pagination, Autoplay } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+
+const images = [
+  {
+    src: '/assets/certifications/discover.jpg',
+    alt: "Discover"
+  },
+  {
+    src: '/assets/certifications/js1.jpg',
+    alt: "JavaScript I"
+  },
+  {
+    src: '/assets/certifications/js2.jpg',
+    alt: "JavaScript II"
+  },
+  {
+    src: '/assets/certifications/js3.jpg',
+    alt: "Javascript III"
+  },
+  {
+    src: '/assets/certifications/js5.jpg',
+    alt: "JavaScript V"
+  },
+  {
+    src: '/assets/certifications/ts.jpg',
+    alt: "TypeScript"
+  },
+  {
+    src: '/assets/certifications/sass.jpg',
+    alt: "Sass"
+  },
+  {
+    src: '/assets/certifications/Gits.jpg',
+    alt: "Git & Github"
+  },
+  {
+    src: '/assets/certifications/nlwia.jpg',
+    alt: "NLW - IA"
+  },
+  {
+    src: '/assets/certifications/nlwnotes.jpg',
+    alt: "NLW - Notes"
+  },
+]
+
 
 export function CertificationsSection() {
   const [navigation, setNavigation] = useState(false)
