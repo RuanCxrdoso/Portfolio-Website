118b1f0392714e28673145fa6de1593c3e2f3e34
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-02-11 20:05:28 -0300

    feat: Certifications carousel

--- src/app/components/CertificationsSection.jsx
+++ src/app/components/CertificationsSection.jsx
@@ -1,10 +1,67 @@
-import js1 from '../../../public/assets/certifications/js2.jpg'
-import js2 from '../../../public/assets/certifications/js2.jpg'
-import js3 from '../../../public/assets/certifications/js3.jpg'
-import js5 from '../../../public/assets/certifications/js5.jpg'
-import ts from '../../../public/assets/certifications/ts.jpg'
-import sass from '../../../public/assets/certifications/sass.jpg'
-import gits from '../../../public/assets/certifications/Gits.jpg'
-import nlwIa from '../../../public/assets/certifications/nlwia.png'
-import nlwnotes from '../../../public/assets/certifications/nlwnotes.jpg'
-import discover from '../../../public/assets/certifications/discover.jpg'
+"use client"
+
+import Image from 'next/image'
+import { images } from '../../../public/certificationsImages/certificationsImages';
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { EffectCoverflow, Navigation, Pagination, Autoplay } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
+export function CertificationsSection() {
+  const swiperStyle = {
+    backgroundImage: 'url()',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center center',
+  }
+  return (
+    <section className='mb-24 mt-16 sm:mb-36'>
+      <h2 className="py-2 text-center text-4xl sm:text-6xl font-bold mt-8 mb-8">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#F2A900] via-[#F28900] to-[#F25900]">Certifications</span>
+      </h2>
+      <div className='h-full sm:mt-16'>
+        <Swiper
+          modules={[Navigation, Pagination, EffectCoverflow, Autoplay]}
+          rewind={true}
+          loop={true}
+          spaceBetween={40}
+          slidesPerView={3}
+          speed={700}
+          navigation={window.innerWidth > 767}
+          effect="coverflow"
+          pagination={{ clickable: true }}
+          coverflowEffect={{
+            rotate: 20,
+            slideShadows: true,
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+              speed: 800,
+            },
+            767: {
+              slidesPerView: 3,
+            },
+          }}
+          >
+          {
+            images.map((image, index) => {
+              return (
+                <SwiperSlide key={index} className='rounded-xl overflow-hidden'>
+                  <Image src={image.src} alt={image.alt} height={500} width={500} />
+                </SwiperSlide>
+              )
+            })
+          }
+        </Swiper>
+      </div>
+    </section>
+  )
+}
