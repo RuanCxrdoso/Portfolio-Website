6c9916936690893ac4d42e279d2b11b9f0c62cfa
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-04-28 16:41:30 -0300

    feat: Navbar side

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -4,20 +4,22 @@ import { NavLink } from "./NavLink"
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 export function MenuOverlay({ links, setNavBarOpen, navOpen }) {
-  const navOpenTransition = navOpen ? 'translate-x-0' : '-translate-x-full'
+  const navOpenTransition = navOpen ? 'translate-x-0' : 'translate-x-full'
 
   return(
     <div className={`${navOpenTransition} bg-black/80 transition-all duration-500 fixed inset-0`}>
-      <button onClick={setNavBarOpen} className="absolute right-4 top-4 flex items-center px-3 py-2 rounded text-slate-20">
+
+      <button onClick={setNavBarOpen} className="absolute left-4 top-4 flex items-center px-3 py-2 rounded text-slate-20">
         <XMarkIcon className="h-10 w-10"/>
       </button>
 
-      <div className={`${navOpenTransition} flex items-center justify-center h-full w-[60%] bg-[#141414] transition-all duration-500 delay-200`}>
-        <ul className="flex flex-col py-4 justify-between items-center h-3/4">
+      <div className={`${navOpenTransition} absolute right-0 flex items-center justify-center h-full w-[70%] bg-[#141414] transition-all duration-500 delay-200`}>
+        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#F2A900] to-transparent rounded-full h-80 w-80 z-0 blur-xl absolute bottom-0 -right-[55%] translate-y-[60%]"></div>
+        <ul className="flex flex-col justify-around items-center h-3/4">
           {
             links.map((link, index) => (
               <li key={index}>
-                <NavLink href={link.path} title={link.title} />
+                <NavLink href={link.path} title={link.title} setNavBarOpen={setNavBarOpen} />
               </li>
             ))
           }
